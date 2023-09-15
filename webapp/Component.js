e8b327809f4e422fb37ab01d839b873a573885a4
Harshita Verma <harshitavermasap@outlook.com>
2023-09-15 16:11:28 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.es5.productslistreport.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
